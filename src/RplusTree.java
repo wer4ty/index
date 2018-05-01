5b9935aefe27938fb23e0cc6289c3a990c23b10f
unknown <wer4ty@gmail.com>
2018-05-02 00:19:04 +0300

    init add data file 2 clases run and rplusTree try to read data file succeed

---
+++ src/RplusTree.java
@@ -0,0 +1,10 @@
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author Server.
+ *         Created May 1, 2018.
+ */
+public class RplusTree {
+
+}
---
+++ src/Run.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author Server.
+ *         Created May 1, 2018.
+ */
+public class Run {
+
+	/**
+	 * TODO Put here a description of what this method does.
+	 *
+	 * @param args
+	 */
+	
+	
+	public static List<String> readLines(File file) throws Exception {
+		int countLines = 0;
+	      if (!file.exists()) {
+	          return new ArrayList<String>();
+	      }
+	      BufferedReader reader = new BufferedReader(new FileReader(file));
+	      List<String> results = new ArrayList<String>();
+	      String line = reader.readLine();
+	      while (line != null) {
+	          results.add(line);
+	          line = reader.readLine();
+	          countLines++;
+	      }
+	      System.out.println(countLines);
+	      return results;
+	  }
+	
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub.
+		File dataFile = new File("./resourse/data.dat");
+		readLines(dataFile);
+	}
+
+}
